926835ffe3e463aefbcb6651b3644954c199a532
fjwcoder <fjwcoder@gmail.com>
2018-04-13 18:07:25 +0800

    完成收货地址的添加

--- app/index/controller/Address.php
+++ app/index/controller/Address.php
@@ -19,11 +19,69 @@ class Address extends controller
         
     }
 
-    public function add(){
+    //获取用户的收货地址
+    public function getAddress(){
+        
+    }
 
+    public function add(){
+        // 1. 获取省份
+        $province = $this->getProvince();
+
+        // 2. 获取城市
+        $currentP = current($province); // 取当前数组值
+        $pid = $currentP['id'];
+        $city = $this->getCity($pid, true);
+
+        // 3. 获取区县
+        $currentC = current($city);
+        $cid = $currentC['id'];
+        $area = $this->getArea($cid, true);
+        
+        $currentA = current($area);
+        $aid = $currentA['id'];
+
+        $this->assign('province', $province);
+        $this->assign('city', $city);
+        $this->assign('area', $area);
+        $this->assign('community', $this->getCommunity($aid, true));
+        $this->assign('station', $this->getStation($aid, true));
+        
         $this->assign('footer', ['status'=>false]);
         return $this->fetch();
         
+    }
+
+    // 保存新增的地址
+    public function addSave(){
+        $address['name'] = input('name', '', 'htmlspecialchars,trim');
+        $address['mobile'] = input('mobile', '', 'htmlspecialchars,trim');
+        $address['province'] = input('province', 0, 'intval');
+        $address['city'] = input('city', 0, 'intval');
+        $address['area'] = input('area', 0, 'intval');
+        $address['address'] = input('address', '', 'htmlspecialchars,trim');
+        $address['community'] = input('community', '', 'htmlspecialchars,trim');
+        $address['station'] = input('station', '', 'htmlspecialchars,trim');
+
+        if(!isMobileNumber($address['mobile'])){
+            return '手机号格式错误'; die;
+        }
+
+        $province = $this->getProvince();
+        $city = $this->getCity($address['province'], true);
+        $area = $this->getArea($address['city'], true);
+        
+        $address['province'] = $province[$address['province']]['title'];
+        $address['city'] = $city[$address['city']]['title'];
+        $address['area'] = $area[$address['area']]['title'];
+        $address['userid'] = 1;//Session::get(Config::get('USER_ID'));
+        $address['type'] = 0;
+        $add = Db::name('user_address') -> insert($address);
+        if($add){
+            return $this->redirect('/index/address/index');
+        }
+
+
     }
 
     public function edit(){
@@ -33,8 +91,127 @@ class Address extends controller
         
     }
 
-    public function region(){
-        
+    public function editSave(){
+
+    }
+
+    // 获取省份
+    public function getProvince(){
+        $province = cache('REGION_PROVINCE');
+        if($province){
+            return $province;
+        }else{
+
+            $province = Db::name('region') -> where(['type'=>1,'active'=>1]) ->order('id') -> column('id, pid, name, title, type, data, link');
+            // $province = Db::name('region') -> where(['type'=>1,'active'=>1]) ->order('id') -> select();
+            if($province){
+                // cache('REGION_PROVINCE', $province);
+                return $province;
+            }else{
+                return [];
+            }
+            
+        }
+    }
+
+    // $pid 上级ID
+    // $controller true: 控制器调用，返回数组； false: 前台调用，返回json
+    public function getCity($pid=0, $controller=false){
+        if($pid === 0){
+            $pid = input('pid', 0, 'intval');
+            
+        }
+
+        if($pid === 0){
+            return 'province参数错误'; die;
+        }
+
+        $city = cache('REGION_CITY');
+        if(empty($city)){
+
+            $city = Db::name('region') -> where(['pid'=>$pid, 'type'=>2,'active'=>1]) -> column('id, pid, name, title,type');
+            if($city){
+                // cache('REGION_CITY', $city);
+            }
+        }
+        if($controller){ // true: 控制器内调用； false: ajax调用
+            return $city;
+        }else{
+            return json_encode($city, JSON_UNESCAPED_UNICODE);
+            
+        }
+
+    }
+
+    public function getArea($pid=0, $controller=false){
+        if($pid === 0){
+            $pid = input('pid', 0, 'intval');
+        }
+
+        if($pid === 0){
+            return 'city参数错误'; die;
+        }
+
+        $area = cache('REGION_AREA');
+        if(empty($area)){
+            $area = Db::name('region') -> where(['pid'=>$pid, 'type'=>3,'active'=>1]) -> column('id, pid, title');
+            if($area){
+                // cache('REGION_AREA', $area);
+            }
+        }
+        if($controller){ // true: 控制器内调用； false: ajax调用
+            return $area;
+        }else{
+            return json_encode($area, JSON_UNESCAPED_UNICODE);
+        }
+    }
+
+
+    public function getCommunity($pid=0, $controller=false){
+        if($pid === 0){
+            $pid = input('pid', 0, 'intval');
+        }
+
+        if($pid === 0){
+            return 'city参数错误'; die;
+        }
+
+        $community = cache('REGION_COMMUNITY');
+        if(empty($community)){
+            $community = Db::name('community') -> where(['pid'=>$pid, 'status'=>1]) -> column('id, pid, title');
+            if($community){
+                // cache('REGION_COMMUNITY', $community);
+            }
+        }
+        if($controller){ // true: 控制器内调用； false: ajax调用
+            return $community;
+        }else{
+            $station = $this->getStation($pid, true);
+            return json_encode(['community'=>$community, 'station'=>$station], JSON_UNESCAPED_UNICODE);
+        }
+    }
+
+    public function getStation($pid, $controller=false){
+        if($pid === 0){
+            $pid = input('pid', 0, 'intval');
+        }
+
+        if($pid === 0){
+            return 'city参数错误'; die;
+        }
+
+        $station = cache('REGION_STATION');
+        if(empty($station)){
+            $station = Db::name('station') -> where(['pid'=>$pid, 'status'=>1]) -> column('id, pid, title');
+            if($station){
+                // cache('REGION_STATION', $community);
+            }
+        }
+        if($controller){ // true: 控制器内调用； false: ajax调用
+            return $station;
+        }else{
+            return json_encode($station, JSON_UNESCAPED_UNICODE);
+        }
     }
 
 }
--- app/index/controller/Goods.php
+++ app/index/controller/Goods.php
@@ -12,10 +12,53 @@ use think\cache\driver\Redis;
 class Goods extends controller
 {
     public function index(){
+        $gid = input('gid', 1, 'intval');
+        if($gid === 0){
+            return '商品ID错误'; die;
+        }
+        // 1. 获取商品详情信息
+        $goods = $this->getGoodsDetail($gid);
+        // return dump($goods);
+        $this->assign('goods', $goods);
+        // 2. 获取默认收货地址
+        
+
+        // 3. 计算预计送达时间
+
 
         $this->assign('footer', ['status'=>false]);
         return $this->fetch();
-        
     }
 
+    # |=================================================================
+    # | 
+    # |
+    # |
+    # |
+    # |=================================================================
+    public function getGoodsDetail($gid=0){
+
+        if($gid===0){
+            return ['status'=>false];
+        }else{
+            // 1. 全部信息 + detail
+            $goods = Db::name('goods') -> alias('a') 
+                ->join('goods_detail b', 'a.id=b.gid', 'LEFT')
+                ->field('a.id, a.catid, a.name, a.sub_name, a.key_words, a.brand, a.service, a.promotion, 
+                    a.price, a.amount, a.sell_amount, a.img, a.weight, a.free_shipping, a.shipping_money, 
+                    a.status, a.description, b.detail ')
+                ->where(['a.status'=>1, 'a.id'=>$gid, 'b.gid'=>$gid])
+                -> find();
+
+            // 2. 图片信息
+            $pictures = Db::name('goods_picture') -> where(['gid'=>$gid]) -> select();
+            // 3. 规格信息
+            $specs = Db::name('goods_spec') -> where(['gid'=>$gid]) ->field('gid, spec, num, price') -> select();
+            
+            $goods['pics'] = $pictures;
+            $goods['specs'] = $specs;
+            
+            return ['status'=>true, 'goods'=>$goods];
+        }
+    }
 }
