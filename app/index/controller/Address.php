715d185ee51260cd7ad6eb47272c736a5ab41985
fjwcoder <fjwcoder@gmail.com>
2018-04-14 11:33:22 +0800

    完成收货地址的增删改查（有bug）

--- app/index/controller/Address.php
+++ app/index/controller/Address.php
@@ -1,7 +1,7 @@
 <?php
 namespace app\index\controller;
 use app\model\model\Region;
-
+use app\index\controller\Common as Common;
 use think\Controller;
 use think\Config;
 use think\Session;
@@ -9,19 +9,60 @@ use think\Db;
 use think\Request;
 use think\cache\driver\Redis;
 
-class Address extends controller
+class Address extends Common
 {
     public function index(){
-
-        $this->assign('address', ['id'=>1]);
+        $address = $this->getAddress(); // 参数为空，获取全部地址
+        $this->assign('address', $address);
         $this->assign('footer', ['status'=>false]);
         return $this->fetch();
         
     }
 
     //获取用户的收货地址
-    public function getAddress(){
-        
+    // $id : 获取的哪一条地址
+    // $default = true : 只显示默认地址
+    public function getAddress($default=false,$id=0){
+        if($id===0){
+            $id = input('id', 0, 'intval');
+        }
+        $where['userid'] = session(config('USER_ID'));
+        if($default){  // 获取默认地址
+            $where['type'] = 1;
+        }else{ 
+            if($id !== 0){ // 获取指定地址
+                $where['id'] = $id;
+            }
+        }
+        $address = Db::name('user_address') -> where($where) -> order('type desc') -> select();
+        if($default || $id !== 0){
+            return $address[0];
+        }else{
+            return $address;
+        }
+        // return ($default)?$address[0]:$address; // 返回全部地址，还是只返回一个
+    }
+
+    public function setDefaultAddress($id=0){
+        if($id===0){
+            $id = input('id', 0, 'intval');
+        }
+        $uid = session(config('USER_ID'));
+        Db::startTrans();
+        try{
+            $refresh = Db::name('user_address')->where(['userid'=>$uid]) -> update(['type'=>0]); 
+            $default = Db::name('user_address') -> where(['userid'=>$uid, 'id'=>$id]) -> update(['type'=>1]);
+            if($default){
+                Db::commit(); 
+                // return $this->redirect('/index/login/index');
+                return $this->redirect('/index/address/index');
+            }else{
+                Db::rollback(); 
+            }
+        }catch (\Exception $e) {
+            Db::rollback();
+        }
+
     }
 
     public function add(){
@@ -37,7 +78,7 @@ class Address extends controller
         $currentC = current($city);
         $cid = $currentC['id'];
         $area = $this->getArea($cid, true);
-        
+
         $currentA = current($area);
         $aid = $currentA['id'];
 
@@ -46,8 +87,9 @@ class Address extends controller
         $this->assign('area', $area);
         $this->assign('community', $this->getCommunity($aid, true));
         $this->assign('station', $this->getStation($aid, true));
-        
+
         $this->assign('footer', ['status'=>false]);
+
         return $this->fetch();
         
     }
@@ -56,9 +98,9 @@ class Address extends controller
     public function addSave(){
         $address['name'] = input('name', '', 'htmlspecialchars,trim');
         $address['mobile'] = input('mobile', '', 'htmlspecialchars,trim');
-        $address['province'] = input('province', 0, 'intval');
-        $address['city'] = input('city', 0, 'intval');
-        $address['area'] = input('area', 0, 'intval');
+        $address['province'] = input('province', '', 'htmlspecialchars,trim');
+        $address['city'] = input('city', '', 'htmlspecialchars,trim');
+        $address['area'] = input('area', '', 'htmlspecialchars,trim');
         $address['address'] = input('address', '', 'htmlspecialchars,trim');
         $address['community'] = input('community', '', 'htmlspecialchars,trim');
         $address['station'] = input('station', '', 'htmlspecialchars,trim');
@@ -67,14 +109,14 @@ class Address extends controller
             return '手机号格式错误'; die;
         }
 
-        $province = $this->getProvince();
-        $city = $this->getCity($address['province'], true);
-        $area = $this->getArea($address['city'], true);
+        // $province = $this->getProvince();
+        // $city = $this->getCity($address['province'], true);
+        // $area = $this->getArea($address['city'], true);
         
-        $address['province'] = $province[$address['province']]['title'];
-        $address['city'] = $city[$address['city']]['title'];
-        $address['area'] = $area[$address['area']]['title'];
-        $address['userid'] = 1;//Session::get(Config::get('USER_ID'));
+        // $address['province'] = $province[$address['province']]['title'];
+        // $address['city'] = $city[$address['city']]['title'];
+        // $address['area'] = $area[$address['area']]['title'];
+        $address['userid'] = Session::get(Config::get('USER_ID'));
         $address['type'] = 0;
         $add = Db::name('user_address') -> insert($address);
         if($add){
@@ -85,14 +127,59 @@ class Address extends controller
     }
 
     public function edit(){
+        $id = input('id', 0, 'intval');
+        $address = $this->getAddress(false, $id);
 
+// return dump($address);
+        // 1. 获取省份
+        $province = $this->getProvince();
+        // 2. 获取城市
+        $currentP = current($province); // 取当前数组值
+        $pid = $currentP['id'];
+        $city = $this->getCity($pid, true);
+        // 3. 获取区县
+        $currentC = current($city);
+        $cid = $currentC['id'];
+        $area = $this->getArea($cid, true);
+        $currentA = current($area);
+        $aid = $currentA['id'];
+        $this->assign('province', $province);
+        $this->assign('city', $city);
+        $this->assign('area', $area);
+
+        $this->assign('community', $this->getCommunity($aid, true));
+        $this->assign('station', $this->getStation($aid, true));
+
+
+        $this->assign('address', $address);
         $this->assign('footer', ['status'=>false]);
         return $this->fetch();
         
     }
 
     public function editSave(){
+        $addressid = input('addressid', 0, 'intval');
 
+        $address['name'] = input('name', '', 'htmlspecialchars,trim');
+        $address['mobile'] = input('mobile', '', 'htmlspecialchars,trim');
+        $address['province'] = input('province', '', 'htmlspecialchars,trim');
+        $address['city'] = input('city', '', 'htmlspecialchars,trim');
+        $address['area'] = input('area', '', 'htmlspecialchars,trim');
+        $address['address'] = input('address', '', 'htmlspecialchars,trim');
+        $address['community'] = input('community', '', 'htmlspecialchars,trim');
+        $address['station'] = input('station', '', 'htmlspecialchars,trim');
+
+        if($addressid === 0){
+            return '参数错误'; die;
+        }
+        if(!isMobileNumber($address['mobile'])){
+            return '手机号格式错误'; die;
+        }
+
+        $update = Db::name('user_address') -> where(['id'=>$addressid]) ->update($address);
+        if($update){
+            return $this->redirect('/index/address/index');
+        }
     }
 
     // 获取省份
@@ -102,7 +189,7 @@ class Address extends controller
             return $province;
         }else{
 
-            $province = Db::name('region') -> where(['type'=>1,'active'=>1]) ->order('id') -> column('id, pid, name, title, type, data, link');
+            $province = Db::name('region') -> where(['type'=>1,'active'=>1]) ->order('id') -> column('title, id, pid, name, type, data, link');
             // $province = Db::name('region') -> where(['type'=>1,'active'=>1]) ->order('id') -> select();
             if($province){
                 // cache('REGION_PROVINCE', $province);
@@ -118,8 +205,9 @@ class Address extends controller
     // $controller true: 控制器调用，返回数组； false: 前台调用，返回json
     public function getCity($pid=0, $controller=false){
         if($pid === 0){
-            $pid = input('pid', 0, 'intval');
-            
+            $title = input('title', '', 'htmlspecialchars,trim'); // 省级title
+            $province = $this->getProvince();
+            $pid = $province[$title]['id'];
         }
 
         if($pid === 0){
@@ -129,7 +217,7 @@ class Address extends controller
         $city = cache('REGION_CITY');
         if(empty($city)){
 
-            $city = Db::name('region') -> where(['pid'=>$pid, 'type'=>2,'active'=>1]) -> column('id, pid, name, title,type');
+            $city = Db::name('region') -> where(['pid'=>$pid, 'type'=>2,'active'=>1]) -> column('title, id, pid, name,type');
             if($city){
                 // cache('REGION_CITY', $city);
             }
@@ -145,7 +233,10 @@ class Address extends controller
 
     public function getArea($pid=0, $controller=false){
         if($pid === 0){
-            $pid = input('pid', 0, 'intval');
+            
+            $title = input('title', '', 'htmlspecialchars,trim'); // 市级的title
+            $city = Db::name('region') -> where(['active'=>1, 'type'=>2, 'title'=>$title]) -> column('id');
+            $pid = $city[0];
         }
 
         if($pid === 0){
@@ -154,11 +245,12 @@ class Address extends controller
 
         $area = cache('REGION_AREA');
         if(empty($area)){
-            $area = Db::name('region') -> where(['pid'=>$pid, 'type'=>3,'active'=>1]) -> column('id, pid, title');
+            $area = Db::name('region') -> where(['pid'=>$pid, 'type'=>3,'active'=>1]) -> column('title, id, pid');
             if($area){
                 // cache('REGION_AREA', $area);
             }
         }
+        
         if($controller){ // true: 控制器内调用； false: ajax调用
             return $area;
         }else{
@@ -169,7 +261,9 @@ class Address extends controller
 
     public function getCommunity($pid=0, $controller=false){
         if($pid === 0){
-            $pid = input('pid', 0, 'intval');
+            $title = input('title', '', 'htmlspecialchars,trim'); // 市级的title
+            $area = Db::name('region') -> where(['active'=>1,'type'=>3, 'title'=>$title]) -> column('id');
+            $pid = $area[0];
         }
 
         if($pid === 0){
@@ -178,7 +272,7 @@ class Address extends controller
 
         $community = cache('REGION_COMMUNITY');
         if(empty($community)){
-            $community = Db::name('community') -> where(['pid'=>$pid, 'status'=>1]) -> column('id, pid, title');
+            $community = Db::name('community') -> where(['pid'=>$pid, 'status'=>1]) -> column('title, id, pid');
             if($community){
                 // cache('REGION_COMMUNITY', $community);
             }
@@ -193,7 +287,9 @@ class Address extends controller
 
     public function getStation($pid, $controller=false){
         if($pid === 0){
-            $pid = input('pid', 0, 'intval');
+            $title = input('title', '', 'htmlspecialchars,trim'); // 市级的title
+            $area = Db::name('region') -> where(['active'=>1,'type'=>3, 'title'=>$title]) -> column('id');
+            $pid = $area[0];
         }
 
         if($pid === 0){
@@ -202,7 +298,7 @@ class Address extends controller
 
         $station = cache('REGION_STATION');
         if(empty($station)){
-            $station = Db::name('station') -> where(['pid'=>$pid, 'status'=>1]) -> column('id, pid, title');
+            $station = Db::name('station') -> where(['pid'=>$pid, 'status'=>1]) -> column('title, id, pid');
             if($station){
                 // cache('REGION_STATION', $community);
             }
@@ -211,6 +307,14 @@ class Address extends controller
             return $station;
         }else{
             return json_encode($station, JSON_UNESCAPED_UNICODE);
+        }
+    }
+
+    public function delAddress(){
+        $id = input('id', 0, 'intval');
+        $del = Db::name('user_address') -> where(['userid'=>session(config('USER_ID')), 'id'=>$id]) -> delete();
+        if($del){
+            return $this->redirect('/index/address/index');
         }
     }
 
